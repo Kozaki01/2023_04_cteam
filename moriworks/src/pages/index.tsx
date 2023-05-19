c274cc581e17ce7593ec5f65550c99e1cf4b4e27
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-19 12:09:43 +0900

    header 確認用

--- moriworks/src/pages/index.tsx
+++ moriworks/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+// import Header from '../components/index/Header/Header';
 
 const HomePage = () => {
   // ページのコンテンツやロジックをここに書く
 
   return (
     <div>
+      {/* <Header type /> */}
       indexページ
     </div>
   );
