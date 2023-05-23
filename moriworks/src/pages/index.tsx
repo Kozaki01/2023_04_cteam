2bec2383bd42dda325f7880e4a5ce673f09957a5
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-05-23 11:47:24 +0900

    タイトルコンポーネント作成

--- moriworks/src/pages/index.tsx
+++ moriworks/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import PageComponent from "../components/index/index"
+import PageComponent from '../components/index/index';
 import Header from '../components/index/Header/Header';
+import Title from '../components/Title';
 
 const Index = () => {
   return (
     <>
       <Header type />
       <PageComponent />
+      <Title text={'My Profile'} />
     </>
   );
 };
