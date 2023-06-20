05adfac7bfd1b357f36a3251f366fff7bf604452
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-20 11:49:02 +0900

    profile取得処理作成

--- moriworks/src/components/Function/DBProfile.tsx
+++ moriworks/src/components/Function/DBProfile.tsx
@@ -1,5 +1,6 @@
 import { supabase } from '../../../client/supabase';
 
+// プロフィールが存在しているかどうか
 export const checkProfileExistence = async (account_id: number) => {
   try {
     const { data, error } = await supabase
@@ -11,10 +12,31 @@ export const checkProfileExistence = async (account_id: number) => {
       throw error;
     }
 
-    console.log(data)
+    console.log(data);
     return data.length > 0;
   } catch (error) {
     console.error('Error checking profile existence:', error);
     return false;
+  }
+};
+
+// プロフィール取得
+export const getProfile = async (account_id: number) => {
+  try {
+    // IDが一致するデータを取得
+    const { data, error } = await supabase
+      .from('profile')
+      .select('profile_id')
+      .eq('account_id', account_id);
+
+    if (error) {
+      throw error;
+    }
+    console.log(data);
+    return data;
+  } catch (error) {
+    // エラーハンドリング
+    console.error('Error get profile:', error);
+    return { error };
   }
 };
