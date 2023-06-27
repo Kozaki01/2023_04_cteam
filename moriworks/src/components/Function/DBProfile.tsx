ddfca80ee76a25337f0310f34c3332ca2d5da588
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-27 11:48:18 +0900

    Profile登録処理

--- moriworks/src/components/Function/DBProfile.tsx
+++ moriworks/src/components/Function/DBProfile.tsx
@@ -1,5 +1,6 @@
 import { supabase } from '../../../client/supabase';
 
+// チェック機能
 export const checkProfileExistence = async (account_id: number) => {
   try {
     const { data, error } = await supabase
@@ -11,10 +12,107 @@ export const checkProfileExistence = async (account_id: number) => {
       throw error;
     }
 
-    console.log(data)
+    console.log(data);
     return data.length > 0;
   } catch (error) {
     console.error('Error checking profile existence:', error);
     return false;
+  }
+};
+
+// プロフィールの作成
+export const createProfile = async (
+  account_id: number,
+  name_user: string,
+  birthday: Date,
+  address: string,
+  self_publicity: string
+) => {
+  try {
+    // Profileテーブルにデータを挿入
+    const { data, error } = await supabase.from('profile').insert({
+      account_id: account_id,
+      name_user: name_user,
+      birthday: birthday,
+      address: address,
+      self_publicity: self_publicity,
+    });
+    if (error) {
+      throw error;
+    }
+    console.log('createProfile:', data);
+    return data;
+  } catch (error) {
+    // エラーハンドリング
+    console.error(error);
+    return { error };
+  }
+};
+
+// profile_idの取得
+export const fetch_id = async (account_id: number) => {
+  try {
+    // アカウントIDが一致するデータを一個取得
+    const { data, error } = await supabase
+      .from('profile')
+      .select('profile_id')
+      .eq('account_id', account_id)
+      .single();
+    // エラーが発生したときcatchに移動
+    if (error) {
+      throw error;
+    }
+    // dataを返す
+    console.log('fetch_id', data);
+    return data;
+  } catch (error) {
+    // エラーハンドリング
+    console.error(error);
+    return { error };
+  }
+};
+
+// 希望地域の登録
+export const createDesiredArea = async (
+  area_id: number,
+  profile_id: number
+) => {
+  try {
+    // 希望地域にデータを登録
+    const { error } = await supabase.from('desired_area').insert({
+      area_id: area_id,
+      profile_id: profile_id,
+    });
+    if (error) {
+      throw error;
+    } else {
+      return { error: false };
+    }
+  } catch (error) {
+    // エラーハンドリング
+    console.error(error);
+    return { error };
+  }
+};
+
+// 希望業種の登録
+export const desired_job_type = async (
+  job_type_id: number,
+  profile_id: number
+) => {
+  try {
+    const { data, error } = await supabase.from('desired_area').insert({
+      area_id: job_type_id,
+      profile_id: profile_id,
+    });
+    if (error) {
+      throw error;
+    } else {
+      return { error: false };
+    }
+  } catch (error) {
+    // エラーハンドリング
+    console.error(error);
+    return { error };
   }
 };
