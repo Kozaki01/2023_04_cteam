7f8fbd8eb752cd98c890c37ef2f538f4e5259bb4
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-30 12:17:49 +0900

    プロフィール取得処理

--- moriworks/src/components/Function/DBProfile.tsx
+++ moriworks/src/components/Function/DBProfile.tsx
@@ -18,6 +18,43 @@ export const checkProfileExistence = async (account_id: number) => {
   }
 };
 
+// プロフィール取得
+export const getProfile = async (account_id: number) => {
+  try {
+    // IDが一致するデータを取得
+    const { data, error } = await supabase
+      .from('profile')
+      .select(
+        `
+        name_user,
+        birthday,
+        address,
+        desired_area {
+          area {
+            area_name,
+          }
+        },
+        desired_job_type {
+          job_type {
+            job_type_name,
+          }
+        }
+        self_publicity,
+        `
+      )
+      .eq('account_id', account_id);
+    if (error) {
+      throw error;
+    }
+    console.log(data);
+    return data;
+  } catch (error) {
+    // エラーハンドリング
+    console.error('Error get profile:', error);
+    return { error };
+  }
+};
+
 // プロフィールの作成
 export const createProfile = async (
   account_id: number,
