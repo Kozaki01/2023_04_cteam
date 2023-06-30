1476868105ad93dd089aaf3e6057a6dfbd1a54c6
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-06-30 12:30:18 +0900

    画面遷移の追加

--- moriworks/src/components/Profile/CreateProfile.tsx
+++ moriworks/src/components/Profile/CreateProfile.tsx
@@ -13,8 +13,8 @@ type btnItem = {
   color: string;
   border: string;
   shadow: string;
-  hovercolor:string
-  hover:string;
+  hover: string;
+  hovercolor: string;
 };
 
 interface props {
@@ -23,6 +23,16 @@ interface props {
 
 const user: React.FC<props> = ({ account_id }) => {
   const router = useRouter();
+
+  // ユーザTopに遷移
+  const moveTop = async () => {
+    router.push('/top_users').then((_) => {});
+  };
+  // ユーザ編集画面に遷移
+  const moveEdit = async () => {
+    router.push('/edit_users').then((_) => {});
+  };
+
   const btns: btnItem[] = [
     {
       title: '戻る',
@@ -33,8 +43,8 @@ const user: React.FC<props> = ({ account_id }) => {
       color: 'black',
       border: '',
       shadow: '',
-      hovercolor:'E5E5E5',
-      hover:'',
+      hover: '',
+      hovercolor: '#E5E5E5',
     }, //戻る
     {
       title: '編集する',
@@ -45,8 +55,8 @@ const user: React.FC<props> = ({ account_id }) => {
       color: 'black',
       border: '',
       shadow: '',
-      hovercolor:'E5E5E5',
-      hover:'',
+      hover: '',
+      hovercolor: '#E5E5E5',
     }, //編集する
   ];
   const btn1Props: btnItem = {
@@ -61,29 +71,58 @@ const user: React.FC<props> = ({ account_id }) => {
       <div className={styles.div1}>
         <div className={styles.div2}>
           <Title text={'My Profile'} />
-            <div>
-              <p className={`${styles.td1} ${styles.padding_top}`}>名前</p>
-              <div className={styles.text_width}><input type="text" placeholder='山田太郎' className={styles.text} ></input></div>
-
-              <div className={`${styles.td2} ${styles.padding_top}`}>生年月日</div>
-              <div className={styles.date_width}><input type="date" ></input></div>
-
-              <div className={`${styles.td1} ${styles.padding_top}`}>住所</div>
-              <div className={styles.text_width}><input type="text" placeholder='岩手県盛岡市中央通り3丁目' className={styles.text}></input></div>
-
-              <div className={`${styles.td2} ${styles.padding_top}`}>希望業種</div>
-              {/* 希望業種のコンポーネント */}
-
-              <div className={`${styles.td2} ${styles.padding_top}`}>希望地域</div>
-              {/* 希望地域のコンポーネント */}
-
-              <div className={`${styles.td3} ${styles.padding_top}`}>自己PR</div>
-              <div className={styles.textarea_size}><textarea placeholder='例:私の強みは〇〇です。'  className={styles.text_area}></textarea></div>
+          <div>
+            <p className={`${styles.td1} ${styles.padding_top}`}>名前</p>
+            <div className={styles.text_width}>
+              <input
+                type="text"
+                placeholder="山田太郎"
+                className={styles.text}
+              ></input>
             </div>
+
+            <div className={`${styles.td2} ${styles.padding_top}`}>
+              生年月日
+            </div>
+            <div className={styles.date_width}>
+              <input type="date"></input>
+            </div>
+
+            <div className={`${styles.td1} ${styles.padding_top}`}>住所</div>
+            <div className={styles.text_width}>
+              <input
+                type="text"
+                placeholder="岩手県盛岡市中央通り3丁目"
+                className={styles.text}
+              ></input>
+            </div>
+
+            <div className={`${styles.td2} ${styles.padding_top}`}>
+              希望業種
+            </div>
+            {/* 希望業種のコンポーネント */}
+
+            <div className={`${styles.td2} ${styles.padding_top}`}>
+              希望地域
+            </div>
+            {/* 希望地域のコンポーネント */}
+
+            <div className={`${styles.td3} ${styles.padding_top}`}>自己PR</div>
+            <div className={styles.textarea_size}>
+              <textarea
+                placeholder="例:私の強みは〇〇です。"
+                className={styles.text_area}
+              ></textarea>
+            </div>
+          </div>
           <div className={styles.flex_btn}>
-            <Btn {...btn1Props} />
+            <div onClick={moveTop}>
+              <Btn {...btn1Props} />
+            </div>
             <span className={styles.space}></span>
-            <Btn {...btn2props} />
+            <div onClick={moveEdit}>
+              <Btn {...btn2props} />
+            </div>
           </div>
         </div>
       </div>
