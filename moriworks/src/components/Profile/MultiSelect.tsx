0b2ddc9ebff409a38abf817447e8ea0f8f1aa454
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-04 12:25:13 +0900

    optionに地域、業種を入れる処理

--- moriworks/src/components/Profile/MultiSelect.tsx
+++ moriworks/src/components/Profile/MultiSelect.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import Select from 'react-select';
 import styles from "./MultiSelect.module.scss";
+import { getArea } from '../Function/DBArea';
+import { getJobType } from '../Function/DBJobType';
+import { error } from 'console';
 
 interface props {
   isArea: boolean
@@ -10,12 +13,43 @@ interface props {
 const MultiSelect: React.FC<props> = ({isArea}) => {
   const [warna, setWarna] = useState([]);
   const [ukuran, setUkuran] = useState([]);
-  const options = [{ value: 'biru', label: 'Biru' }];
-  if (isArea) {
-    // 地域一覧を取得してoptionに追加する
-    
+  const options:any = [];
+
+  // optionsの中身を取得
+  const getOptions = async() => {
+    if (isArea) {
+      // 地域一覧を取得
+      const areaResult = await getArea()
+      // エラーじゃないとき
+      if (!areaResult.error) {
+        // areaに取得した配列を入れる
+        const area = areaResult.data;
+        // 空じゃないとき
+        if (area != undefined) {
+          // 配列に値を追加
+          area.forEach(element => {
+            options.push({"value":element.area_id, "label":element.area_name })
+          });
+        }
+      }
+    } else {
+      // 業種一覧を取得
+      const JobResult = await getJobType()
+      // エラーじゃないとき
+      if (!JobResult.error) {
+        // jobに取得した配列を入れる
+        const job = JobResult.data;
+        // 空じゃないとき
+        if (job != undefined) {
+          // 配列に値を追加
+          job.forEach(element => {
+            options.push({"value":element.job_type_id, "label":element.job_type_name })
+          });
+        }
+      }
+    }
   }
-  
+  getOptions()
 
   const handleWarnaChange = async (selected: any, selectaction: any) => {
     const { action } = selectaction;
