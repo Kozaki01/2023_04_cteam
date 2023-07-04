2c848157d570db11fe0ea919c2f1af67b562ae32
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-07-04 12:24:23 +0900

    業種一覧の取得処理の作成

---
+++ moriworks/src/components/Function/DBJobType.tsx
@@ -0,0 +1,22 @@
+import { supabase } from '../../../client/supabase';
+
+// 業種一覧の取得
+export const getJobType = async () => {
+  try {
+    // 業種取得
+    const { data, error } = await supabase.from('job_type').select();
+    
+    if (error) {
+      throw error
+    }
+    if (data && data.length > 0) {
+      // console.log(data);
+      return { data };
+    } else {
+      throw new Error('アカウントが見つかりません。');
+    }
+  } catch (error) {
+    console.error(error);
+    return {error};
+  }
+}
