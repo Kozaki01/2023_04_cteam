8ffddc7788113de7f1868d7c5dcbc6848164df8c
Kozaki01 <101164560+Kozaki01@users.noreply.github.com>
2023-07-11 12:09:36 +0900

    起動時に必要だったものを削除

--- moriworks/src/pages/index.tsx
+++ moriworks/src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import PageComponent from '../components/index/index';
-import Title from '../components/Title';
-
 
 const Index = () => {
   return (
     <>
       <PageComponent />
-      <Title text={"Edit Profile"}/>
     </>
   );
 };
