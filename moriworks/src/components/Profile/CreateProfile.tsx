3bf512eec6cc5321a87c328e343d9004d813ebf7
t-dounoma <t.dounomae.sys21@morijyobi.ac.jp>
2023-07-14 10:52:57 +0900

    [feat] top_adminの追加

--- moriworks/src/components/Profile/CreateProfile.tsx
+++ moriworks/src/components/Profile/CreateProfile.tsx
@@ -4,11 +4,9 @@ import { useRouter } from 'next/router';
 import Title from '../Title';
 import Btn from './../index/TopButton/TopButton';
 import MultiSelect from './MultiSelect';
-import { createProfile } from '../Function/DBProfile';
-import { fetch_id } from '../Function/DBProfile';
+import { createProfile, checkProfileExistence, fetch_id } from '../Function/DBProfile';
 import { createDesiredArea } from '../Function/DBDesiredArea';
 import { createDesiredJobType } from './../Function/DBDesiredJobType';
-import { checkProfileExistence } from '../Function/DBProfile';
 
 type btnItem = {
   title: string;
@@ -23,18 +21,17 @@ type btnItem = {
   hover: string;
 };
 
-interface props {
-  account_id: number;
-}
+interface props {}
 
-const CreateProfile: React.FC<props> = ({ account_id }) => {
+const CreateProfile: React.FC<props> = () => {
+  const account_id = localStorage.getItem('account_id');
   const router = useRouter();
-  const [name, setName] = useState(String); // 名前
-  const [birthday, setBirthday] = useState(new Date()); // 生年月日
-  const [address, setAddress] = useState(String); // 住所
-  const [area, setArea] = useState([]); // 希望地域
-  const [job, setJob] = useState([]); // 希望業種
-  const [pr, setPr] = useState(String); // 自己PR
+  const [name, setName] = useState<string>(''); // 名前
+  const [birthday, setBirthday] = useState<Date>(new Date()); // 生年月日
+  const [address, setAddress] = useState<string>(''); // 住所
+  const [area, setArea] = useState<any[]>([]); // 希望地域
+  const [job, setJob] = useState<any[]>([]); // 希望業種
+  const [pr, setPr] = useState<string>(''); // 自己PR
 
   // リダイレクトの処理
   React.useEffect(() => {
@@ -62,16 +59,16 @@ const CreateProfile: React.FC<props> = ({ account_id }) => {
   /**
    * input値の取得
    */
-  // nameの取得
-  const changeName = (e: any) => {
-    setName(e.target.value);
-  };
+    // nameの取得
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.value);
+    };
   // 生年月日の取得
-  const changeBirthday = (e: any) => {
-    setBirthday(e.target.value);
+  const changeBirthday = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBirthday(new Date(e.target.value));
   };
   // 住所の取得
-  const changeAddress = (e: any) => {
+  const changeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAddress(e.target.value);
   };
   // 希望地域の取得
@@ -97,16 +94,16 @@ const CreateProfile: React.FC<props> = ({ account_id }) => {
     setJob(selected);
   };
   // 自己ｐｒの取得
-  const changePr = (e: any) => {
+  const changePr = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setPr(e.target.value);
   };
 
   // プロフィール作成処理
   const doCreate = async () => {
-    const result = await createProfile(account_id, name, birthday, address, pr);
+    const result = await createProfile(Number(account_id), name, birthday, address, pr);
     // 作成出来た時
     if (!result.error) {
-      const profile_id = await fetch_id(account_id);
+      const profile_id = await fetch_id(Number(account_id));
       if (!profile_id.error) {
         area.forEach((item) => {
           createDesiredArea(item['value'], profile_id);
@@ -160,72 +157,72 @@ const CreateProfile: React.FC<props> = ({ account_id }) => {
           <Title text={'アカウント作成'} />
           <table className={styles.table1}>
             <tbody>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_name1} ${styles.td1}`}>名前</td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_name2}>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_name1} ${styles.td1}`}>名前</td>
+              <td className={`${styles.colon} ${styles.text_box}`}>:</td>
+              <td className={styles.td_name2}>
+                <input
+                  type="text"
+                  placeholder="山田太郎"
+                  className={styles.text}
+                  onChange={changeName}
+                />
+              </td>
+            </tr>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_birth1} ${styles.td1}`}>
+                生年月日
+              </td>
+              <td className={styles.colon}>:</td>
+              <td className={styles.td_birth2}>
+                <label className={styles.date}>
                   <input
-                    type="text"
-                    placeholder="山田太郎"
-                    className={styles.text}
-                    onChange={changeName}
+                    type="date"
+                    className={styles.input_birth}
+                    onChange={changeBirthday}
                   />
-                </td>
-              </tr>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_birth1} ${styles.td1}`}>
-                  生年月日
-                </td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_birth2}>
-                  <label className={styles.date}>
-                    <input
-                      type="date"
-                      className={styles.input_birth}
-                      onChange={changeBirthday}
-                    />
-                  </label>
-                </td>
-              </tr>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_add1} ${styles.td1}`}>住所</td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_add2}>
-                  <input
-                    type="text"
-                    placeholder="岩手県盛岡市中央通り3丁目"
-                    className={styles.text}
-                    onChange={changeAddress}
-                  />
-                </td>
-              </tr>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_area1} ${styles.td1}`}>希望地域</td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_area2}>
-                  {/* 希望地域のコンポーネント */}
-                  <MultiSelect isArea={true} changeEvent={changeArea} />
-                </td>
-              </tr>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_job1} ${styles.td1}`}>希望業種</td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_job2}>
-                  {/* 希望業種のコンポーネント */}
-                  <MultiSelect isArea={false} changeEvent={changeJob} />
-                </td>
-              </tr>
-              <tr className={styles.tr1}>
-                <td className={`${styles.td_pr1} ${styles.td1}`}>自己PR</td>
-                <td className={styles.colon}>:</td>
-                <td className={styles.td_pr2}>
+                </label>
+              </td>
+            </tr>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_add1} ${styles.td1}`}>住所</td>
+              <td className={styles.colon}>:</td>
+              <td className={styles.td_add2}>
+                <input
+                  type="text"
+                  placeholder="岩手県盛岡市中央通り3丁目"
+                  className={styles.text}
+                  onChange={changeAddress}
+                />
+              </td>
+            </tr>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_area1} ${styles.td1}`}>希望地域</td>
+              <td className={styles.colon}>:</td>
+              <td className={styles.td_area2}>
+                {/* 希望地域のコンポーネント */}
+                <MultiSelect isArea={true} changeEvent={changeArea} />
+              </td>
+            </tr>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_job1} ${styles.td1}`}>希望業種</td>
+              <td className={styles.colon}>:</td>
+              <td className={styles.td_job2}>
+                {/* 希望業種のコンポーネント */}
+                <MultiSelect isArea={false} changeEvent={changeJob} />
+              </td>
+            </tr>
+            <tr className={styles.tr1}>
+              <td className={`${styles.td_pr1} ${styles.td1}`}>自己PR</td>
+              <td className={styles.colon}>:</td>
+              <td className={styles.td_pr2}>
                   <textarea
                     placeholder="例:私の強みは〇〇です。"
                     className={styles.text_area}
                     onChange={changePr}
                   ></textarea>
-                </td>
-              </tr>
+              </td>
+            </tr>
             </tbody>
           </table>
           <div className={styles.flex_btn}>
---
+++ moriworks/src/pages/top_admin.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PageComponent from '../components/top/top_admin';
+import Header from '../components/Header/Header';
+import Btn from "../components/Btn";
+import { useRouter } from 'next/router';
+
+const Index = () => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const  checkLocalStorage =async () => {
+      const accountId = localStorage.getItem('account_id');
+      if (!accountId || accountId.trim() === '') {
+        await router.push("/");
+      }
+    };
+
+    checkLocalStorage();
+  }, []);
+
+  return (
+    <>
+      <Header type={false} />
+      <PageComponent />
+    </>
+  );
+};
+
+export default Index;
